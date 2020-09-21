1bec5dc747863f7dbcf4a3ac3266279676c7a150
Jeremy Walker <jez.walker@gmail.com>
2020-09-21 14:43:43 +0100

    Add mentor inbox component

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -15,6 +15,8 @@ import { initReact } from './react_bootloader.jsx'
 import { ExampleIterationsSummaryTable } from '../components/example/iterations_summary_table.jsx'
 import { MaintainingIterationsSummaryTable } from '../components/maintaining/iterations_summary_table.jsx'
 import { NotificationIcon } from '../components/notifications/icon.jsx'
+import { MentorConversationsList } from '../components/mentoring/mentor_conversations_list.jsx'
+>>>>>>> ca17745... Add mentor inbox component
 
 // Add all react components here.
 // Each should map 1-1 to a component in app/helpers/components
@@ -29,6 +31,13 @@ initReact({
     <MaintainingIterationsSummaryTable iterations={data.iterations} />
   ),
   'notification-icon': (data) => <NotificationIcon count={data.count} />,
+  'mentor-conversations-list': (data) => (
+    <MentorConversationsList
+      endpoint={data.endpoint}
+      urlParams={data.url_params}
+      retryParams={data.retry_params}
+    />
+  ),
 })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
